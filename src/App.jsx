1e9c2e9296b21085de9edd2a453ef7f373bdf722
Shadyx6 <shadkhanxox>
2025-03-26 10:12:37 +0530

    Fix for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,14 @@ import LocomotiveScroll from 'locomotive-scroll';
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
   return (
+  <div className="h-full w-screen relative overflow-x-hidden">
+    <div className="h-full w-full lg:hidden absolute bg-[#18181B] z-50 flex items-center justify-center">
+  <div className="text-white text-3xl md:text-4xl font-bold text-center p-6 rounded-lg bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg">
+    <h2 className='fixed text-base   top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Please use a desktop or larger screen to continue.💻</h2>
+  </div>
+</div>
     <div className="main w-full bg-[#18181B] ">
+      
       <Navbar />
       <Work />
       <Cards />
@@ -22,6 +29,7 @@ function App() {
       <Tabs />
       <Footer />
     </div>
+  </div>
    
   )
 }
