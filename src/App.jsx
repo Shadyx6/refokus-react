7f34d33a86d9d5c3cc4bee2e758be041047eeb22
Shadyx6 <shadkhanxox>
2025-03-26 15:22:25 +0530

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   <div className="h-full w-screen relative overflow-x-hidden">
     <div className="h-full w-full lg:hidden absolute bg-[#18181B] z-50 flex items-center justify-center">
   <div className="text-white text-3xl md:text-4xl font-bold text-center p-6 rounded-lg bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg">
-    <h2 className='fixed text-base   top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Please use a desktop or larger screen to continue.💻</h2>
+    <h2 className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Please use a desktop or larger screen to continue.💻</h2>
   </div>
 </div>
     <div className="main w-full bg-[#18181B] ">
